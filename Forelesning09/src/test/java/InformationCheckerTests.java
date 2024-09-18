1c698d5c6027b713a0f4a32f744cd6242860f852
Ole-Edvard Ørebæk <oleedvao@hiof.no>
2024-09-18 12:49:25 +0200

    Added comment to InformationCheckerTests

--- Forelesning09/src/test/java/InformationCheckerTests.java
+++ Forelesning09/src/test/java/InformationCheckerTests.java
@@ -2,6 +2,18 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+/*
+Dette er en test klasse som er dedikert til å teste funksjonalitet i klassen InformationChecker og inneholder en
+rekke forskjellige enhetstester.
+En enhetstest er egentlig bare en void-metode, men vi merker dem som enhetsteser for JUnit ved å benytte taggen
+@Test. Vi kan også gi hver enhetstest dislplaynavn ved bruk av taggen @DisplayName. Vi bør uansett være flinke til å
+enhetstesten (metoden) et beskrivende navn for hva det tester.
+Husk at vi bør lage en unik enhetstest for hver "equivalence partion", altså for hver kategori av input mulig til
+enheten (både riktig og ikke-riktige).
+Under er det definert tre enhetstester for InformationChecker sin metode controlAge sine equivalence partions.
+Husk også at hver enhetstest bør teste flere input-verdier slik at den (forhåpentligvis) er genraliserbar for andre
+tilsvarende input-verdier.
+ */
 public class InformationCheckerTests {
 
     @Test
