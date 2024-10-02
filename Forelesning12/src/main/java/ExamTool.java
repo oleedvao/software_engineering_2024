0d44bb8f827bc305ab00af9562b038a856f3bfa8
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2024-10-02 10:56:21 +0200

    F12 - 6 - refactor

--- Forelesning12/src/main/java/ExamTool.java
+++ Forelesning12/src/main/java/ExamTool.java
@@ -2,9 +2,7 @@ import java.util.ArrayList;
 
 public abstract class ExamTool {
     public static int countGradeInList(String grade, ArrayList<String> grades) {
-        if (grade == "A") {
-            return 3;
-        }
-        else return 2;
+        if (grade.equals("A")) return 3;
+        return 2;
     }
 }
