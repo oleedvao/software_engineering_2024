4b5912a5123cd40614df82fe87b2bbf246c0fe86
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2024-10-02 11:21:43 +0200

    F12 - 10 - green

--- Forelesning12/src/main/java/ExamTool.java
+++ Forelesning12/src/main/java/ExamTool.java
@@ -2,8 +2,17 @@ import java.util.ArrayList;
 
 public abstract class ExamTool {
     public static int countGradeInList(String grade, ArrayList<String> grades) {
+        /*
         if (grade.equals("A")) return 3;
         else if (grade.equals("B")) return 2;
         return 1;
+         */
+        int count = 0;
+        for (String gradeX : grades) {
+            if (gradeX.equals(grade)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
