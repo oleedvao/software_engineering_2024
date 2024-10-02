c23ecfea6c4796951517f434e819a5df96708e8e
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2024-10-02 11:02:21 +0200

    F12 - 9 - red

--- Forelesning12/src/test/java/ExamToolTests.java
+++ Forelesning12/src/test/java/ExamToolTests.java
@@ -14,14 +14,23 @@ public class ExamToolTests {
         ArrayList<String> grades = new ArrayList<>();
         Collections.addAll(grades,
                 "A", "B", "B", "A", "C", "D", "A");
+        ArrayList<String> grades2 = new ArrayList<>();
+        Collections.addAll(grades2,
+                "B", "D", "D", "A", "C", "C");
 
         int countA = ExamTool.countGradeInList("A", grades);
         int countB = ExamTool.countGradeInList("B", grades);
         int countC = ExamTool.countGradeInList("C", grades);
+        // ...
+
+        int countA2 = ExamTool.countGradeInList("A", grades2);
 
         Assertions.assertEquals(3, countA);
         Assertions.assertEquals(2, countB);
         Assertions.assertEquals(1, countC);
+        //...
+
+        Assertions.assertEquals(1, countA2);
 
     }
 
