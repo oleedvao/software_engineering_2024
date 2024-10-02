b01365110bf0357d02feb4d24c0a95b35abdc93e
Ole-Edvard Ørebæk <oleedvao@hiof.no>
2024-10-02 13:04:00 +0200

    F12 - Final comments

--- Forelesning12/src/main/java/ExamTool.java
+++ Forelesning12/src/main/java/ExamTool.java
@@ -1,6 +1,12 @@
 import java.util.ArrayList;
 
 public abstract class ExamTool {
+
+    /*
+    Denne enheten er fullstendig skrevet ut ifra TDD.
+    Merk at den ikke ble definert til å kunne generelt håndtere telling av flere unike lister før testene uttrykte et
+    behov for dette ("triangulation). Før dette ble testene oppnådd med enkle if-tester.
+     */
     public static int countGradeInList(String gradeToCount, ArrayList<String> grades) {
         int count = 0;
         for (String gradeInList : grades) {
--- Forelesning12/src/test/java/ExamToolTests.java
+++ Forelesning12/src/test/java/ExamToolTests.java
@@ -9,10 +9,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ExamToolTests {
 
+    /*
+    Se commitene navngitt F12 for en stegvis liste med endringer tilsvarende stegene i Test Driven Development (red,
+    green, refactor).
+    Disse stegvise endringene virker kanskje banale og unødvendig små, men banker inn dette med å teste ofte og all
+    koden, samt å ofte tenke på refaktorering for å sørge for at resultatet blir "god kode".
+    Generelt er TDD nyttig i tilfeller hvor man står fast og ikke helt vet hvordan man skal implementere den relevante
+    enheten. I slike tilfeller kan TDD veilede deg i riktig retning ved å gjøre små og overkommelige steg.
+     */
     @Test
     @DisplayName("Count grade in list of grades")
     public void countGradeInList() {
 
+        //Arrange
         ArrayList<String> grades = new ArrayList<>();
         Collections.addAll(grades,
                 "A", "B", "B", "A", "C", "D", "A");
@@ -20,6 +29,7 @@ public class ExamToolTests {
         Collections.addAll(grades2,
                 "B", "D", "D", "A", "C", "C");
 
+        //Act
         int countA = ExamTool.countGradeInList("A", grades);
         int countB = ExamTool.countGradeInList("B", grades);
         int countC = ExamTool.countGradeInList("C", grades);
@@ -34,6 +44,7 @@ public class ExamToolTests {
         int countE2 = ExamTool.countGradeInList("E", grades2);
         int countF2 = ExamTool.countGradeInList("F", grades2);
 
+        //Assert
         assertEquals(3, countA);
         assertEquals(2, countB);
         assertEquals(1, countC);
