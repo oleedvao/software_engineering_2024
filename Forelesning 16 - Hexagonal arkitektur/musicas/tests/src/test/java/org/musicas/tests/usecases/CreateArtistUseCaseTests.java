600c25335f0e23c6cb882bcd340b3f33f0f9a476
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2024-10-18 13:29:47 +0200

    Fixed NullPointerException
    Solution was missing @ExtendsWith(MockitoExtention.class)

--- Forelesning 16 - Hexagonal arkitektur/musicas/tests/src/test/java/org/musicas/tests/usecases/CreateArtistUseCaseTests.java
+++ Forelesning 16 - Hexagonal arkitektur/musicas/tests/src/test/java/org/musicas/tests/usecases/CreateArtistUseCaseTests.java
@@ -2,13 +2,16 @@ package org.musicas.tests.usecases;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.musicas.core.domain.Artist;
 import org.musicas.core.dto.CreateArtistUseCaseDTO;
 import org.musicas.core.port.MusicRepository;
 import org.musicas.core.usecase.CreateArtistUseCase;
 
+@ExtendWith(MockitoExtension.class)
 public class CreateArtistUseCaseTests {
 
     @Mock
