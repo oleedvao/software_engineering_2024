6311fe3d2358dceb920c61e169f74c316c071b6e
Ole-Edvard Ørebæk <oleedvao@hiof.no>
2024-10-23 11:10:19 +0200

    added intentional error in test

--- Forelesning16-Hexagonal_arkitektur/musicas/tests/src/test/java/org/musicas/tests/usecases/GetArtistUseCaseTests.java
+++ Forelesning16-Hexagonal_arkitektur/musicas/tests/src/test/java/org/musicas/tests/usecases/GetArtistUseCaseTests.java
@@ -42,7 +42,7 @@ public class GetArtistUseCaseTests {
         ArtistDTO result = getArtist.execute(dto);
 
         // Assert
-        assertEquals("Radiohead", result.getName());
+        assertEquals("Radiohead1", result.getName());
         assertEquals(1, result.getAlbums().size());
         assertEquals("In Rainbows", result.getAlbums().get(0).getTitle());
     }
